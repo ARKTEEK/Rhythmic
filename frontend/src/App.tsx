8833492cce18ceb25bbd890d021dcad5736fef81
ARKTEEK <minde398@gmail.com>
2025-03-23 16:04:52 +0200

    chore: switch to Vite, React + TS and TailwindCSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="py10 h-screen">
+      <h1 className="font-bold text-3x1 text-center">Todos</h1>
+    </main>
+  )
+}
+
+export default App
