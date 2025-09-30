c63852c326dd45cd26cbe582c84d39bc13d70858
ARKTEEK <minde398@gmail.com>
2025-09-30 16:35:23 +0300

    feat(frontend): Allow 3 accounts per provider

--- frontend/src/pages/private/ConnectionsPage.tsx
+++ frontend/src/pages/private/ConnectionsPage.tsx
@@ -43,10 +43,10 @@ export default function ConnectionsPage() {
 
   return (
     <div className="p-6">
-      <div className="grid grid-cols-3 gap-8 max-w-7xl mx-auto mt-8">
+      <div className="grid grid-cols-3 gap-8 max-w-7xl mx-auto">
         { platformsWithAccounts.map((platform) => (
           <Window
-            containerClassName={ "h-[380px] w-[400px]" }
+            containerClassName={ "h-[400px] w-[380px]" }
             key={ platform.name }
             ribbonClassName={ platform.ribbonColor }
             windowClassName={ `${ platform.windowColor }` }>
@@ -59,7 +59,7 @@ export default function ConnectionsPage() {
                   <h3 className="text-2xl font-bold text-gray-900">{ platform.name }</h3>
                   <div className="flex items-center space-x-2">
                     <span className="text-sm text-gray-600">
-                      { platform.accounts.length } accounts
+                      { platform.accounts.length }/3 accounts
                     </span>
                   </div>
                 </div>
@@ -91,15 +91,17 @@ export default function ConnectionsPage() {
 
             <button
               onClick={ platform.redirect }
-              className={ `box-style-md w-full py-3 border-2 border-brown-800 ${
-                platform.ribbonColor || "bg-brown-200"
-              } hover:opacity-80 transition-all duration-200 group hover:cursor-pointer font-bold text-brown-900 mt-auto` }>
-              <div
-                className="flex items-center justify-center space-x-2">
+              disabled={ platform.accounts.length >= 3 }
+              className={ `box-style-md w-full py-3 border-2 border-brown-800
+                           ${ platform.ribbonColor || "bg-brown-200" }
+                           transition-all duration-200 group font-bold text-brown-900 mt-auto
+                           ${ platform.accounts.length >= 3 ? "opacity-50 cursor-not-allowed" : "hover:opacity-80 hover:cursor-pointer" }` }>
+              <div className="flex items-center justify-center space-x-2">
                 <FaPlus className="text-brown-900"/>
                 <span>Add Account</span>
               </div>
             </button>
+
           </Window>
         )) }
       </div>
