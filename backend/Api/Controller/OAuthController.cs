6c12b728125634548352fa0702fe0c234c87b235
ARKTEEK <minde398@gmail.com>
2025-10-01 12:40:32 +0300

    feat(backend): Make OAuthController work with every platform

--- backend/Api/Controller/OAuthController.cs
+++ backend/Api/Controller/OAuthController.cs
@@ -9,42 +9,44 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Api.Controller;
 
 [ApiController]
-[Route("api")]
+[Route("api/oauth")]
 public class OAuthController : ControllerBase {
   private readonly IOAuthService _iOAuthService;
-  private readonly IAccountTokensService _accountTokensService;
   private readonly UserManager<User> _userManager;
 
-  public OAuthController(IOAuthService iOAuthService, IAccountTokensService accountTokensService,
-    UserManager<User> userManager) {
+  public OAuthController(IOAuthService iOAuthService, UserManager<User> userManager) {
     _iOAuthService = iOAuthService;
-    _accountTokensService = accountTokensService;
     _userManager = userManager;
   }
 
-  [HttpGet("oauth/google/login")]
-  public IActionResult GoogleLogin() {
-    string loginUrl = _iOAuthService.GetLoginUrl(OAuthProvider.Google);
+  [HttpGet("{provider}/login")]
+  public IActionResult Login([FromRoute] string provider) {
+    if (!Enum.TryParse<OAuthProvider>(provider, true, out var providerEnum)) {
+      return BadRequest(new { error = "Unsupported provider." });
+    }
+
+    string loginUrl = _iOAuthService.GetLoginUrl(providerEnum);
     return Redirect(loginUrl);
   }
 
-  [HttpPost("oauth/google/callback")]
-  public async Task<IActionResult> GoogleLogin([FromBody] OAuthLoginRequestDto request) {
+  [HttpPost("{provider}/callback")]
+  public async Task<IActionResult> Callback([FromRoute] string provider,
+    [FromBody] OAuthLoginRequestDto request) {
+    if (!Enum.TryParse<OAuthProvider>(provider, true, out var providerEnum)) {
+      return BadRequest(new { error = "Unsupported provider." });
+    }
+
     User? user = await this.GetCurrentUserAsync(_userManager);
+    if (user == null) {
+      return Unauthorized();
+    }
 
     OAuthLoginResponseDto result = await _iOAuthService.LoginAsync(
       user.Id,
-      OAuthProvider.Google,
+      providerEnum,
       request.Code
     );
 
     return Ok(result);
-  }
-
-  [HttpDelete("oauth/google/logout")]
-  public async Task<IActionResult> GoogleLogout() {
-    User? user = await this.GetCurrentUserAsync(_userManager);
-    await _accountTokensService.DeleteAsync(user.Id, OAuthProvider.Google);
-    return Ok(new { success = true });
   }
 }
