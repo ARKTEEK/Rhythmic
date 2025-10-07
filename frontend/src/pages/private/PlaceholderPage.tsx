ea663fe418d516b034ca28a5a747fee729e3d2b6
ARKTEEK <minde398@gmail.com>
2025-10-07 18:08:54 +0300

    chore: Remove content from Placeholder page

--- frontend/src/pages/private/PlaceholderPage.tsx
+++ frontend/src/pages/private/PlaceholderPage.tsx
@@ -1,12 +1,5 @@
-import LoadingWindow from "../../components/ui/Window/LoadingWindow.tsx";
-
 export default function PlaceholderPage() {
   return (
-    <LoadingWindow
-      status={ "error" }
-      loadingMessage={ `Linking your account...` }
-      errorMessage="Authentication failed! Please try again...."
-      completeMessage="Authentication successful."
-    />
+    <div>Placeholder Page</div>
   );
 }
