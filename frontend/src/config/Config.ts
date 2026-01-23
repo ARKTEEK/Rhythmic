d7caae356ab6e6d669eae1b5bb583cc63e1ac748
ARKTEEK <minde398@gmail.com>
2026-01-23 13:18:24 +0200

    fix(frontend): Wrong variable name

--- frontend/src/config/Config.ts
+++ frontend/src/config/Config.ts
@@ -1,6 +1,6 @@
 const defaultApiBaseUrl = "https://127.0.0.1:7184/api";
 
-export const API_BASE_URL = import.meta.env.API_BASE_URL ?? defaultApiBaseUrl;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? defaultApiBaseUrl;
 
 export const googleOAuth = () => {
   window.location.href = `${ API_BASE_URL }/oauth/google/login`;
